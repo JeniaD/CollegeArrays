06097a8024ec1eeb508d803f7a78fe5e478a6e41
JeniaD <53406085+JeniaD@users.noreply.github.com>
2023-10-27 10:15:13 +0000

    Added .data section

--- main.asm
+++ main.asm
@@ -1,17 +1,5 @@
 bits 64
 
-global  _start
+section .data
+    array dd 1, 2, 3; Оголошення масиву з трьох цілих чисел
 
-section .text
-    _start:     
-        mov       rax, 1
-        mov       rdi, 1
-        mov       rsi, message
-        mov       rdx, 13
-        syscall
-        mov       rax, 60
-        xor       rdi, rdi
-        syscall
-
-section   .data
-    message:    db    "Hello, World!", 10
